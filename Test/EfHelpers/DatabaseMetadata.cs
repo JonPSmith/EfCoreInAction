99273c52c170fd624fb6f019428f788eb71bc38d
Jon Smith <jon.smith@selectiveanalytics.com>
2017-03-30 10:47:25 +0100

    Added helper + tests for seeing database settings

---
+++ Test/EfHelpers/DatabaseMetadata.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace test.EfHelpers
+{
+    public static class DatabaseMetadata
+    {
+        public static string GetTableName<TEntity>(this DbContext context)
+        {
+            var efType = context.Model.FindEntityType(typeof(TEntity).FullName);
+            return efType.Relational().TableName;
+        }
+
+        public static string GetColumnName<TEntity, TProperty>(this DbContext context, TEntity source, 
+            Expression<Func<TEntity, TProperty>> model) where TEntity : class
+        {
+            var efType = context.Model.FindEntityType(typeof(TEntity).FullName);
+            var propInfo = GetPropertyInfoFromLambda(model);
+            return efType.FindProperty(propInfo.Name).Relational().ColumnName;
+        }
+
+        public static string GetColumnRelationalType<TEntity, TProperty>(this DbContext context, 
+            TEntity source, Expression<Func<TEntity, TProperty>> model) where TEntity : class
+        {
+            var efType = context.Model.FindEntityType(typeof(TEntity).FullName);
+            var propInfo = GetPropertyInfoFromLambda(model);
+            var relational = efType.FindProperty(propInfo.Name).Relational();
+            return relational.ColumnType;
+        }
+
+        public static string GetColumnSqliteType<TEntity, TProperty>(this DbContext context, TEntity source, 
+            Expression<Func<TEntity, TProperty>> model) where TEntity : class
+        {
+            var efType = context.Model.FindEntityType(typeof(TEntity).FullName);
+            var propInfo = GetPropertyInfoFromLambda(model);
+            var relational = efType.FindProperty(propInfo.Name).Sqlite();
+            return relational.ColumnType;
+        }
+
+        //---------------------------------------------------
+        //private methods
+
+        private static PropertyInfo GetPropertyInfoFromLambda<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> model) where TEntity : class
+        {
+            var memberEx = (MemberExpression)model.Body;
+            if (memberEx == null)
+                throw new ArgumentNullException("model", "You must supply a LINQ expression that is a property.");
+
+            var propInfo = typeof(TEntity).GetProperty(memberEx.Member.Name);
+            if (propInfo == null)
+                throw new ArgumentNullException("model", "The member you gave is not a property.");
+            return propInfo;
+        }
+    }
+}
---
+++ Test/UnitTests/Tests/Ch06_DatabaseMetadata.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataLayer.EfClasses;
+using DataLayer.EfCode;
+using test.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace test.UnitTests.Tests
+{
+    public class Ch06_DatabaseMetadata
+    {
+        [Fact]
+        public void TestGetTableNameOk()
+        {
+            //SETUP
+            var inMemDb = new SqliteInMemory();
+
+            //ATTEMPT
+            using (var context = inMemDb.GetContextWithSetup())
+            {
+                var tableName = context.GetTableName<Book>();
+
+                //VERIFY
+                tableName.ShouldEqual("Books");
+            }
+        }
+
+        [Fact]
+        public void TestGetColumnNameOk()
+        {
+            //SETUP
+            var inMemDb = new SqliteInMemory();
+
+            //ATTEMPT
+            using (var context = inMemDb.GetContextWithSetup())
+            {
+                var colName = context.GetColumnName(new Book(), p => p.BookId);
+
+                //VERIFY
+                colName.ShouldEqual("BookId");
+            }
+        }
+
+        [Fact]
+        public void TestGetColumnRelationalTypeOk()
+        {
+            //SETUP
+            var inMemDb = new SqliteInMemory();
+
+            //ATTEMPT
+            using (var context = inMemDb.GetContextWithSetup())
+            {
+                var colType = context.GetColumnRelationalType(new Book(), p => p.Description);
+
+                //VERIFY
+                colType.ShouldEqual("nvarchar(max)");
+            }
+        }
+
+        [Fact]
+        public void TestGetColumnSqliteTypeTypeOk()
+        {
+            //SETUP
+            var inMemDb = new SqliteInMemory();
+
+            //ATTEMPT
+            using (var context = inMemDb.GetContextWithSetup())
+            {
+                var colType = context.GetColumnSqliteType(new Book(), p => p.PublishedOn);
+
+                //VERIFY
+                colType.ShouldEqual("date");
+            }
+        }
+
+        [Fact]
+        public void TestGetColumnRelationalTypeSQlDatabase()
+        {
+            //SETUP
+            var options =
+                this.ClassUniqueDatabaseSeeded4Books();
+
+            using (var context = new EfCoreContext(options))
+            {
+                //ATTEMPT
+                var colType = context.GetColumnRelationalType(new Book(), p => p.Description);
+
+                //VERIFY
+                colType.ShouldEqual("nvarchar(max)");
+            }
+        }
+
+    }
+}
