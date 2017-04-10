babbe5052f7f3cfbf12f1aaa4d2e6091631bf6c1
Jon Smith <jon.smith@selectiveanalytics.com>
2017-04-10 10:28:10 +0100

    Changed annotations

--- DataLayer/EfCode/Configurations/BookConfig.cs
+++ DataLayer/EfCode/Configurations/BookConfig.cs
@@ -12,36 +12,30 @@ namespace DataLayer.EfCode.Configurations
         public static void Configure
             (this EntityTypeBuilder<Book> entity)
         {
-            entity.HasIndex(x => x.PublishedOn);//Add a non-unique index to the publish date
+            entity.HasIndex(x => x.PublishedOn);
 
-            entity.Property(p => p.PublishedOn) //#A
-                .HasColumnType("date"); //#A
+            entity.Property(p => p.PublishedOn)
+                .HasColumnType("date");        
 
-            entity.Property(p => p.Price) //#B
-                .HasColumnType("decimal(9,2)"); //#B
+            entity.Property(p => p.Price)      
+                .HasColumnType("decimal(9,2)");
 
-            entity.Property(x => x.ImageUrl) //#C
-                .IsUnicode(false); //#C
+            entity.Property(x => x.ImageUrl)
+                .IsUnicode(false);          
 
             //----------------------------
             //relationships
 
-            //entity.HasOne(p => p.Promotion)       //#D
-            //    .WithOne();                       //#D
+            entity.HasOne(p => p.Promotion) //#A
+                .WithOne();                 //#A
 
-            //entity.HasMany(p => p.Reviews)        //#E
-            //    .WithOne()                        //#E
-            //    .HasForeignKey(p => p.BookId);    //#E
-
-            entity.HasMany(p => p.AuthorsLink)
-                .WithOne(p => p.Book);
+            entity.HasMany(p => p.Reviews)     //#B
+                .WithOne()                     //#B
+                .HasForeignKey(p => p.BookId); //#B
         }
     }
     /************************************************************
-    #A Setting the database type to just store the date, instead of the nromal datetime2(7)
-    #B Setting a smaller precision and scale or (9,2) instead of the default (18,2)
-    #C The ImageUrl does not have a Unicode characters in it so we select a smaller string type, varchar
-    #D This defines the One-to-ZeroOrOne relationship to the promotion that a book can optionally have
-    #E This defines the One-to-Many relationship, with a book having zero to many reviews
+    #A This defines the One-to-ZeroOrOne relationship to the promotion that a book can optionally have
+    #B This defines the One-to-Many relationship, with a book having zero to many reviews
      * ***********************************************************/
 }
