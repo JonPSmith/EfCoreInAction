c4e1f603446b99326bf65d5564881ac22030586f
Jon Smith <jon.smith@selectiveanalytics.com>
2017-04-20 15:10:08 +0100

    Ran tests on what properties are included or excluded

---
+++ Test/UnitTests/DataLayer/Ch06_IncludedExcluded.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2016 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using test.EfHelpers;
+using Test.Chapter06Listings;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace test.UnitTests.DataLayer
+{
+    public class Ch06_IncludedExcluded
+    {
+        //private readonly ITestOutputHelper _output;
+
+        //public Ch06_ShadowProperties(ITestOutputHelper output)
+        //{
+        //    _output = output;
+        //}
+
+        [Fact]
+        public void TestIncludedPropertiesOk()
+        {
+            //SETUP
+            using (var context = new Chapter06DbContext(
+                SqliteInMemory.CreateOptions<Chapter06DbContext>()))
+            {
+                {
+                    //var logs = new List<string>();
+                    //SqliteInMemory.SetupLogging(context, logs);
+                    //context.Database.EnsureCreated();
+
+                    //ATTEMPT
+                    var props = context.GetProperties<MyEntityClass>().Select(x => x.Name).ToList();
+
+                    //VERIFY
+                    props.ShouldEqual(new List<string>{ "MyEntityClassId", "InDatabaseProp", "InternalSet", "PrivateSet", "UpdatedOn" });
+                }
+            }
+        }
+
+        
+
+    }
+}
