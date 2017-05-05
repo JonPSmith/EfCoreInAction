55454a99ccd6b2fcec86de7927ed5ec75853118a
Jon Smith <jon.smith@selectiveanalytics.com>
2017-05-05 11:11:42 +0100

    Added concurrency

---
+++ Test/Chapter08Listings/EfClasses/ConcurrencyAuthor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Chapter08Listings.EfClasses
+{
+    public class ConcurrencyAuthor
+    {
+        public int ConcurrencyAuthorId { get; set; }
+
+        public string Name { get; set; }
+
+        [Timestamp] //#A
+        public byte[] RowVersion { get; set; }
+    }
+    /***********************************************
+    #A This marks the property RowVersion as as a timestamp. This will cause the database server to mark it as a ROWVERSION and EF Core will check this when updating to see if this has changed
+     * *********************************************/
+}
