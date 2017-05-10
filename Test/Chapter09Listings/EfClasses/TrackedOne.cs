1eb9c3627dfde73250d4d2a4e368fec1fb7301db
Jon Smith <jon.smith@selectiveanalytics.com>
2017-05-10 09:35:16 +0100

    Made OneToOne optional, as is handled differently

--- Test/Chapter09Listings/EfClasses/TrackedOne.cs
+++ Test/Chapter09Listings/EfClasses/TrackedOne.cs
@@ -7,6 +7,6 @@ namespace Test.Chapter09Listings.EfClasses
     {
         public int Id { get; set; }
 
-        public int TrackedEntityId { get; set; }
+        public int? TrackedEntityId { get; set; }
     }
 }
