089028ab9e6ec276888c4bffdd3fe48fdbb14629
Jon Smith <jon.smith@selectiveanalytics.com>
2017-05-19 10:57:04 +0100

    Added code to help with book, pluse DeteckChanges performance

---
+++ Test/UnitTests/DataLayer/Ch09_ChangeTrackingPerformance.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) 2016 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore;
+using test.EfHelpers;
+using Test.Chapter09Listings.EfClasses;
+using Test.Chapter09Listings.EfCode;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace test.UnitTests.DataLayer
+{
+    public class Ch09_ChangeTrackingPerformance
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Ch09_ChangeTrackingPerformance(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void TestSaveChangesPerformanceBooksOk(int numBooks)
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<EfCoreContext>();
+
+            using (var context = new EfCoreContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.SeedDatabaseDummyBooks(numBooks);
+            }
+            using (var context = new EfCoreContext(options))
+            {
+                var books = context.Books
+                    .Include(x => x.AuthorsLink).ThenInclude(x => x.Author)
+                    .Include(x => x.Reviews)
+                    .ToList();
+
+                //ATTEMPT
+                var timer = new Stopwatch();
+                timer.Start();
+                context.SaveChanges();
+                timer.Stop();
+
+                //VERIFY
+                _output.WriteLine("#{0:####0} books: total time = {1} ms ", numBooks,
+                    timer.ElapsedMilliseconds);
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void TestSaveChangesPerformanceMyEntityOk(int numEntities)
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter09DbContext>();
+
+            using (var context = new Chapter09DbContext(options))
+            {
+                context.Database.EnsureCreated();
+                var entities = new List<MyEntity>();
+                for (int i = 0; i < numEntities; i++)
+                {
+                    var entity = new MyEntity();
+                    entities.Add(entity);
+                }
+                context.AddRange(entities);
+                context.SaveChanges();
+
+            }
+            using (var context = new Chapter09DbContext(options))
+            {
+                var entities = context.MyEntities.ToList();
+                entities.Count.ShouldEqual(numEntities);
+
+                //ATTEMPT
+                var timer = new Stopwatch();
+                timer.Start();
+                context.SaveChanges();
+                timer.Stop();
+
+                //VERIFY
+                _output.WriteLine("#{0:####0} entities: total time = {1} ms ", numEntities,
+                    timer.ElapsedMilliseconds);
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void TestSaveChangesPerformanceNotifyEntityOk(int numEntities)
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter09DbContext>();
+
+            using (var context = new Chapter09DbContext(options))
+            {
+                context.Database.EnsureCreated();
+                var entities = new List<NotifyEntity>();
+                for (int i = 0; i < numEntities; i++)
+                {
+                    var entity = new NotifyEntity();
+                    entities.Add(entity);
+                }
+                context.AddRange(entities);
+                context.SaveChanges();
+
+            }
+            using (var context = new Chapter09DbContext(options))
+            {
+                var entities = context.Notify.ToList();
+                entities.Count.ShouldEqual(numEntities);
+
+                //ATTEMPT
+                var timer = new Stopwatch();
+                timer.Start();
+                context.SaveChanges();
+                timer.Stop();
+
+                //VERIFY
+                _output.WriteLine("#{0:####0} entities: total time = {1} ms ", numEntities,
+                    timer.ElapsedMilliseconds);
+            }
+        }
+    }
+}
---
+++ Test/UnitTests/DataLayer/Ch09_TrackChangesDemo.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) 2016 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using test.EfHelpers;
+using Test.Chapter09Listings.EfClasses;
+using Test.Chapter09Listings.EfCode;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace test.UnitTests.DataLayer
+{
+    public class Ch09_TrackChangesDemo
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Ch09_TrackChangesDemo(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestEntityScalarStagesOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter09DbContext>();
+
+            using (var context = new Chapter09DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                //ATTEMPT && Verify
+                var entity = new MyEntity();
+                entity.MyString = "Test";
+                context.GetEntityState(entity).ShouldEqual(EntityState.Detached);
+                context.Add(entity);
+                context.GetEntityState(entity).ShouldEqual(EntityState.Added);
+                context.SaveChanges();
+                context.GetEntityState(entity).ShouldEqual(EntityState.Unchanged);
+                entity.MyString = "New String";
+                context.GetEntityState(entity).ShouldEqual(EntityState.Modified);
+                context.SaveChanges();
+                context.GetEntityState(entity).ShouldEqual(EntityState.Unchanged);
+                context.Remove(entity);
+                context.GetEntityState(entity).ShouldEqual(EntityState.Deleted);
+                context.SaveChanges();
+                context.GetEntityState(entity).ShouldEqual(EntityState.Detached);
+            }
+        }
+
+        [Fact]
+        public void TestModifyNewOneToOneStagesOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter09DbContext>();
+
+            using (var context = new Chapter09DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                var entity = new MyEntity();
+                entity.MyString = "Test";
+                context.Add(entity);
+                context.SaveChanges();
+            }
+            using (var context = new Chapter09DbContext(options))
+            {
+                //ATTEMPT
+                var entity = 
+                    context.MyEntities.First();
+                var oneToOne = new OneEntity();
+                entity.OneToOne = oneToOne;
+
+                //VERIFY
+                context.NumTrackedEntities().ShouldEqual(2);
+                context.GetEntityState(entity).ShouldEqual(EntityState.Unchanged);
+                context.GetEntityState(oneToOne).ShouldEqual(EntityState.Added);
+                context.GetAllPropsNavsIsModified(entity).ShouldEqual("");
+                context.GetAllPropsNavsIsModified(oneToOne).ShouldEqual("");
+            }
+        }
+
+        [Fact]
+        public void TestModifyExistingOneToOneStagesOk()
+        {
+            //SETUP
+            var options = SqliteInMemory.CreateOptions<Chapter09DbContext>();
+
+            using (var context = new Chapter09DbContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                var entity = new MyEntity {MyString = "Test"};
+                context.Add(entity);
+                context.Add(new OneEntity());
+                context.SaveChanges();
+            }
+            using (var context = new Chapter09DbContext(options))
+            {
+                //ATTEMPT
+                var entity =
+                    context.MyEntities.First();
+                var oneToOne = 
+                    context.OneEntities.First();
+                entity.OneToOne = oneToOne;
+
+                //VERIFY
+                context.NumTrackedEntities().ShouldEqual(2);
+                context.GetEntityState(entity).ShouldEqual(EntityState.Unchanged);
+                context.GetEntityState(oneToOne).ShouldEqual(EntityState.Modified);
+                context.GetAllPropsNavsIsModified(entity).ShouldEqual("OneToOne");
+                context.GetAllPropsNavsIsModified(oneToOne).ShouldEqual("MyEntityId");
+            }
+        }
+    }
+}
