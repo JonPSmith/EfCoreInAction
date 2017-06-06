ff833b6699b5bdd1c9f3f25a920dc6236fe22c63
Jon Smith <jon.smith@selectiveanalytics.com>
2017-06-06 11:42:22 +0100

    Updated annotations

--- Test/Chapter10Listings/EfCode/BookDddRepository.cs
+++ Test/Chapter10Listings/EfCode/BookDddRepository.cs
@@ -5,8 +5,6 @@ using System;
 using System.Linq;
 using DataLayer.QueryObjects;
 using Microsoft.EntityFrameworkCore;
-using ServiceLayer.BookServices;
-using ServiceLayer.BookServices.QueryObjects;
 using Test.Chapter10Listings.EfClasses;
 using Test.Chapter10Listings.QueryObjects;
 
@@ -40,17 +38,16 @@ namespace Test.Chapter10Listings.EfCode
             return true;
         }
 
-        public IQueryable<BookListDto> GetBookList( //#E
-            SortFilterPageOptions options)
+        public IQueryable<BookDdd> GetBookList( //#E
+            DddSortFilterPageOptions options)
         {
             var booksQuery = _context.Books
                 .AsNoTracking()
-                .MapBookDddToDto() //#F
-                .OrderBooksBy(options.OrderByOptions)
-                .FilterBooksBy(options.FilterBy,
+                .OrderBooksBy(options.OrderByOptions) //#F
+                .FilterBooksBy(options.FilterBy,  //#F
                     options.FilterValue);
 
-            options.SetupRestOfDto(booksQuery);
+            options.SetupRestOfDto(booksQuery); //#F
 
             return booksQuery.Page(options.PageNum - 1,
                 options.PageSize);
@@ -61,7 +58,7 @@ namespace Test.Chapter10Listings.EfCode
     #B This simply adds the book to the context
     #C This finds an existing book using its primary key
     #D This tries to delete the book with the given primary key. It returns true if it found a book to delete, ot false if it didn't find a book with that primary key
-    #E This is the DDD equivalent to the ListBooksService class found in the ServiceLayer. It applies a select query, sorts and filters based on the content of the options passed in
-    #F This is the only change from how the ListBooksService class works, simply because I used a different entity name
+    #E This is the DDD equivalent to the ListBooksService class, but it passes back IQueryable<BookDdd> rather than the IQueryable<BoolListDto> that the original verision did
+    #F These query objects are copies of the query objects in the original, non-DDD, design. They use the IQueryable<BookDdd> rather that the original IQueryable<BoolListDto>
      * ****************************************************/
 }
