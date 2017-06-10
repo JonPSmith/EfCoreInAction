29ebbc451e3a1168763dcd377d2636d846639fa1
Jon Smith <jon.smith@selectiveanalytics.com>
2017-06-10 14:48:45 +0100

    Changed property name from RowVersion to ChangeCheck as it was confusing in the book

--- Test/Chapter08Listings/EfClasses/ConcurrencyAuthor.cs
+++ Test/Chapter08Listings/EfClasses/ConcurrencyAuthor.cs
@@ -12,9 +12,9 @@ namespace Test.Chapter08Listings.EfClasses
         public string Name { get; set; }
 
         [Timestamp] //#A
-        public byte[] RowVersion { get; set; }
+        public byte[] ChangeCheck { get; set; }
     }
     /***********************************************
-    #A This marks the property RowVersion as as a timestamp. This will cause the database server to mark it as a ROWVERSION and EF Core will check this when updating to see if this has changed
+    #A This marks the property ChangeCheck as as a timestamp. This will cause the database server to mark it as a SQL ROWVERSION and EF Core will check this when updating to see if this has changed
      * *********************************************/
 }
