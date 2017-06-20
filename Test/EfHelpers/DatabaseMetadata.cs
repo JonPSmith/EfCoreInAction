9bddfaef35b8c35ba3d24fdc8a59a2f02f55db1f
Jon Smith <jon.smith@selectiveanalytics.com>
2017-06-20 19:41:54 +0100

    Removed the incorrect OldGetTableNamesInOrderForDelete

--- Test/EfHelpers/DatabaseMetadata.cs
+++ Test/EfHelpers/DatabaseMetadata.cs
@@ -23,57 +23,6 @@ namespace test.EfHelpers
             return relational.TableName;
         }
 
-        //NOTE: This will not handle a circular relationship: e.g. EntityA->EntityB->EntityA
-        public static  IEnumerable<string> 
-            OldGetTableNamesInOrderForDelete //#A
-                (this DbContext context)
-        {
-            var cannotWipe = context.Model.GetEntityTypes() //#B
-                .FirstOrDefault(x => x.GetForeignKeys()     //#B
-                .SingleOrDefault(y => y.PrincipalEntityType == x) //#B
-                ?.DeleteBehavior == DeleteBehavior.Restrict); //#B
-            if (cannotWipe != null)
-                throw new InvalidOperationException(
-                    $"You cannot delete all the {cannotWipe} rows in one go.");
-
-            var principals = context.Model.GetEntityTypes()    //#C
-                .SelectMany(x => x.GetForeignKeys()            //#C
-                .Where(y => y.PrincipalEntityType != x)        //#C
-                .Select(y => y.PrincipalEntityType)).Distinct()//#C
-                .ToList();                                     //#C
-
-            principals.Sort((itemA, itemB) => //#D
-                {
-                    var fKeysA = itemA.GetForeignKeys()
-                        .ToList();
-                    if (fKeysA.Any(
-                        x => x.PrincipalEntityType == itemB))
-                        return -1; //#E
-                    var fKeysB = itemB.GetForeignKeys().ToList();
-                    if (fKeysB.Any(
-                        x => x.PrincipalEntityType == itemA))
-                        return 1; //#F
-                    return 0; //#G
-                }
-            );
-            var allEntitiesInOrder =                  //#H
-                context.Model.GetEntityTypes()        //#H
-                .Where(x => !principals.Contains(x))  //#H
-                .Union(principals);                   //#H
-            return allEntitiesInOrder.Select(x => x.Relational().TableName);//#I
-        }
-        /************************************************************************
-        #A This method looks at the relationships and returns the tables names so that the dependent entities come before the principal entities
-        #B I catch the hierarchical case where an entity refers to itself - if the delete behavior of this foreign key is set to restrict then you cannot simply delete all the rows in one go
-        #C I extract all the principal entities to sort. The dependent entities don't take part in the sort, as we know they must come at the beginning
-        #D Now I sort the principal entities in case I have the case of EntityA -> EntityB -> EntityC. In that case EntityB must be deleted before EntityA
-        #E If itemA's foreign keys point to itemB, then I must delete itemA first to make sure I don't have a cascade delete problems
-        #F If itemB's foreign keys point to itemA, then I must delete itemB first to make sure I don't have a cascade delete problems
-        #G Otherwise the order doesn't matter
-        #H I now produce combined list with the dependants at the front and the principals at the back
-        #I Finally I extract the table names from the ordered list
-         * ***********************************************************************/
-
         public static IEnumerable<IProperty> GetProperties<TEntity>(this DbContext context)
         {
             var efType = context.Model.FindEntityType(typeof(TEntity).FullName);
