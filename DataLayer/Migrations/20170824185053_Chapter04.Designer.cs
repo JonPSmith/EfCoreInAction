eae9cfce4a04d918989bd9fbfcabeefba1335555
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-24 19:53:23 +0100

    Rebuilt chapter04 migration to include changes in chapter02 model

--- DataLayer/Migrations/20170824185053_Chapter04.Designer.cs
+++ DataLayer/Migrations/20170824185053_Chapter04.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(EfCoreContext))]
-    partial class EfCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20170824185053_Chapter04")]
+    partial class Chapter04
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
