cc20f3172a849ad388147d54e7f252c3fdef9486
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-24 09:40:25 +0100

    Changed to EF Core 2.0.0 in-memory database with unique name

--- Test/EfHelpers/EfInMemory.cs
+++ Test/EfHelpers/EfInMemory.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2016 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT licence. See License.txt in the project root for license information.
 
+using System;
 using DataLayer.EfCode;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,10 +18,9 @@ namespace test.EfHelpers
                 .AddEntityFrameworkInMemoryDatabase()
                 .BuildServiceProvider();
 
-            // Create a new options instance telling the context to use an
-            // InMemory database and the new service provider.
+            // Create a new options instance telling the context to use an in-memory database
             var builder = new DbContextOptionsBuilder<EfCoreContext>();
-            builder.UseInMemoryDatabase()
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString()) //the database name is set to a unique Guid
                    .UseInternalServiceProvider(serviceProvider);
 
             return builder.Options;
