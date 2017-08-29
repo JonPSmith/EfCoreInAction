7b050190e612afd3fce44e81fa2ebd8e7b7a209b
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-29 08:35:11 +0100

    Removed code for EF Core issue #8137

--- Test/Chapter07Listings/EFCode/Configurations/EmployeeShortFkConfig.cs
+++ Test/Chapter07Listings/EFCode/Configurations/EmployeeShortFkConfig.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT licence. See License.txt in the project root for license information.
 
-using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Test.Chapter07Listings.EfClasses;
 
