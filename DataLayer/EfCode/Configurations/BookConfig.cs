c31f51b6f5923b85b337bf2d72e5ad5e4de7d117
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-29 17:47:57 +0100

    Changed config setup to use IEntityTypeConfiguration<T>

---
+++ DataLayer/EfCode/Configurations/BookConfig.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using DataLayer.EfClasses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataLayer.EfCode.Configurations
+{
+    internal class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure
+            (EntityTypeBuilder<Book> entity)
+        {
+            entity.HasIndex(x => x.PublishedOn);
+
+            entity.Property(p => p.PublishedOn)//#A
+                .HasColumnType("date");        
+
+            entity.Property(p => p.Price) //#B
+                .HasColumnType("decimal(9,2)");
+
+            entity.Property(x => x.ImageUrl) //#C
+                .IsUnicode(false);
+
+            //Model-level query filter
+
+            entity
+                .HasQueryFilter(p => !p.SoftDeleted);
+
+            //----------------------------
+            //relationships
+
+            entity.HasOne(p => p.Promotion) //#A
+                .WithOne() //#A
+                .HasForeignKey<PriceOffer>(p => p.BookId); //#A
+
+            entity.HasMany(p => p.Reviews)     //#B
+                .WithOne()                     //#B
+                .HasForeignKey(p => p.BookId); //#B
+        }
+    }
+    /*Type/Size setting**********************************************
+    #A The convention-based mapping for .NET DateTime is SQL datetime2. This command changes the SQL column type to date, which only holds the date, not time
+    #B I set a smaller precision and scale of (9,2) for the price instead of the default (18,2)
+    #C The convention-based mapping for .NET string is SQL nvarchar (16 bit Unicode). This command changes the SQL column type to varchar (8 bit ASCII)
+    * ******************************************************/
+    /*CH07********************************************************
+    #A This defines the One-to-One relationship to the promotion that a book can optionally have. The foreign key is in the PriceOffer
+    #B This defines the One-to-Many relationship, with a book having zero to many reviews
+     * ***********************************************************/
+}
--- DataLayer/EfCode/EfCoreContext.cs
+++ DataLayer/EfCode/EfCoreContext.cs
@@ -3,7 +3,7 @@
 
 using DataLayer.EfClasses;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+using DataLayer.EfCode.Configurations;
 
 namespace DataLayer.EfCode
 {
@@ -21,52 +21,12 @@ namespace DataLayer.EfCode
         protected override void
             OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Book>()  //Add a non-unique index to the publish date
-                .HasIndex(x => x.PublishedOn);
-
-            modelBuilder.Entity<Book>() //#A
-                .Property(x => x.PublishedOn)
-                .HasColumnType("date");
-
-            modelBuilder.Entity<Book>()
-                .Property(p => p.Price) //#B
-                .HasColumnType("decimal(9,2)");
-
-            modelBuilder.Entity<Book>() //#C
-                .Property(x => x.ImageUrl)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Book>()
-                .HasKey(x => x.BookId); //#A
-
-            modelBuilder.Entity<BookAuthor>()          
-                .HasKey(x => new {x.BookId, x.AuthorId}); //#B
-
-            modelBuilder.Entity<PriceOffer>()
-                .Property(p => p.NewPrice)
-                .HasColumnType("decimal(9,2)");
-
-            modelBuilder.Entity<LineItem>()        //#B
-                .HasOne(p => p.ChosenBook)         //#B
-                .WithMany()                        //#B
-                .OnDelete(DeleteBehavior.Restrict);//#B
-
-            modelBuilder.Entity<Book>()
-                .HasQueryFilter(p => !p.SoftDeleted);
+            modelBuilder.ApplyConfiguration(new BookConfig());
+            modelBuilder.ApplyConfiguration(new BookAuthorConfig());
+            modelBuilder.ApplyConfiguration(new PriceOfferConfig());
+            modelBuilder.ApplyConfiguration(new LineItemConfig());
         }                                                 
     }
-    /*Type/Size seeting**********************************************
-    #A The convention-based mapping for .NET DateTime is SQL datetime2. This command changes the SQL column type to date, which only holds the date, not time
-    #B I set a smaller precision and scale of (9,2) for the price instead of the default (18,2)
-    #C The convention-based mapping for .NET string is SQL nvarchar (16 bit Unicode). This command changes the SQL column type to varchar (8 bit ASCII)
-    * ******************************************************/
-    /*Primary key settings**********************************************
-    #A Here I define a single primary key
-    #B Here I use an anonymous object to define two (or more) properties to form a composite key. The order in which the properties appear in the anonymous object defines their order
-    * ******************************************************/
-    /**** Model query filter *************************************
-    #A This adds a filter to all accesses to the Book entities. You can bypass this filter by using the IgnoreQueryFilters() operator
-     * **********************************************************/
 }
 
 /******************************************************************************
