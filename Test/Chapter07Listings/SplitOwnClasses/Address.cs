9732a09147b4266107e5d2b1470260ba91f918b8
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-30 20:13:32 +0100

    Added annotations

--- Test/Chapter07Listings/SplitOwnClasses/Address.cs
+++ Test/Chapter07Listings/SplitOwnClasses/Address.cs
@@ -2,11 +2,14 @@
 // Licensed under MIT licence. See License.txt in the project root for license information.
 namespace Test.Chapter07Listings.SplitOwnClasses
 {
-    public class Address
+    public class Address //#C
     {
         public string NumberAndStreet { get; set; }
         public string City { get; set; }
         public string ZipPostCode { get; set; }
         public string CountryCodeIso2 { get; set; }
     }
+    /****************************************************
+    #C A owned type has no primary key - this type of class is refered to as a 'Value Object' in Domain-Driven Design terms
+     * **************************************************/
 }
--- Test/Chapter07Listings/SplitOwnClasses/OrderInfo.cs
+++ Test/Chapter07Listings/SplitOwnClasses/OrderInfo.cs
@@ -2,12 +2,18 @@
 // Licensed under MIT licence. See License.txt in the project root for license information.
 namespace Test.Chapter07Listings.SplitOwnClasses
 {
-    public class OrderInfo
+    public class OrderInfo //#A
     {
         public int OrderInfoId { get; set; }
         public string OrderNumber { get; set; }
 
-        public Address BillingAddress { get; set; }
-        public Address DeliveryAddress { get; set; }
+        public Address BillingAddress { get; set; } //#B
+        public Address DeliveryAddress { get; set; } //#B
     }
+
+
+    /**********************************************************
+    #A The entity class OrderInfo, with a primary key and two addresses
+    #B There are two, distinct Address classes. The data for each address class will be included in the table that the OrderInfo is mapped to
+     * *********************************************************/
 }
