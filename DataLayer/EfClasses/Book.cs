9fa5ae7f7e40848ab89b88132c99597b48dd0b33
Jon Smith <jon.smith@selectiveanalytics.com>
2017-09-18 10:18:14 +0100

    Added annotations

--- DataLayer/EfClasses/Book.cs
+++ DataLayer/EfClasses/Book.cs
@@ -28,10 +28,10 @@ namespace DataLayer.EfClasses
         public DateTime PublishedOn { get; set; }
         [MaxLength(64)] //#B
         public string Publisher { get; set; }
-        public decimal ActualPrice { get; private set; }
-        public decimal OrgPrice { get; private set; }
+        public decimal OrgPrice { get; private set; } //#A
+        public decimal ActualPrice { get; private set; } //#A
         [MaxLength(PromotionalTextLength)]
-        public string PromotionalText { get; private set; }
+        public string PromotionalText { get; private set; } //#A
         public bool HasPromotion => PromotionalText != null;
         [MaxLength(512)] //#B
         public string ImageUrl { get; set; }
@@ -69,15 +69,16 @@ namespace DataLayer.EfClasses
         /// <param name="imageUrl"></param>
         /// <param name="authors"></param>
         /// <param name="authorsString"></param>
-        public Book(string title, string description, DateTime publishedOn, string publisher, decimal orgPrice, string imageUrl, 
+        public Book(string title, string description, DateTime publishedOn, //#B
+            string publisher, decimal orgPrice, string imageUrl, 
             ICollection<Author> authors, string authorsString = null)
         {
             Title = title;
             Description = description;
             PublishedOn = publishedOn;
             Publisher = publisher;
-            ActualPrice = orgPrice;
-            OrgPrice = orgPrice;
+            OrgPrice = orgPrice; //#C
+            ActualPrice = OrgPrice; //#D
             ImageUrl = imageUrl;
 
             ReviewsCount = 0;
@@ -91,18 +92,54 @@ namespace DataLayer.EfClasses
         //------------------------------------------
         //Action methods
 
+        /// <summary>
+        /// This sets up a promotion
+        /// It assumes OrgPrice was set 
+        /// </summary>
+        /// <param name="newPrice"></param>
+        /// <param name="promotionalText"></param>
+        /// <returns>string conatining error, or null if no error</returns>
+        public string AddPromotion(decimal newPrice, //#E
+            string promotionalText)                  //#E
+        {
+            if (promotionalText == null) //#F
+                return 
+            "You must provide some text to go with the promotion";
+
+            ActualPrice = newPrice;  //#G
+            PromotionalText = promotionalText; //#H
+            return null; //#I
+        }
+
+        public void RemovePromotion() //#J
+        {
+            ActualPrice = OrgPrice; //#K
+            PromotionalText = null; //#L
+        }
+        /******************************************************************
+        #A The properties that control the price all have a private setter so that only the Book entity can change their values
+        #B The only public way to create a Book entity is now via this constructor
+        #C I set the OrgPrice with the recommended retail price of the book
+        #D I also set the ActualPrice to the OrgPrice because a new book starts off without any promotion
+        #E This method adds a price promotion. It returns null if successful, or an error message if there was an error
+        #F A promotion is deemed to be in place of the PromotionalText property isn't null, so we need to check this
+        #G I replace the current ActualPrice with the new, promotional price
+        #H I also set the PromotionalText property, which then tells the rest of the system that a propmotion is in place
+        #I I return null to say that it was successful
+        #J This method removes a price promotion 
+        #K This sets the book's ActualPrice to the recommended retail price held in the OrgPrice property
+        #L It also nulls the PromotionalText, which tells the rest of the system that there isn't a price promotion on this book
+         * ****************************************************************/
+
+
         public void AddReview(DbContext context,
             int numStars, string comment, string voterName) 
         {
-
-
             context.Entry(this)
                 .Collection(c => c.Reviews).Load();
             AddReviewWhenYouKnowReviewCollectionIsLoaded(numStars, comment, voterName);
-
         }
 
-        //This is for 
         public void AddReviewWhenYouKnowReviewCollectionIsLoaded(int numStars, string comment, string voterName)
         {
             var review = new Review(numStars, comment, voterName);
@@ -110,7 +147,6 @@ namespace DataLayer.EfClasses
             AverageVotes = _reviews.Average(x => x.NumStars);
             ReviewsCount = _reviews.Count;
         }
-
 
         public void RemoveReview(DbContext context, Review review) //#G
         {
@@ -124,27 +160,7 @@ namespace DataLayer.EfClasses
             ReviewsCount = _reviews.Count;
         }
 
-        /// <summary>
-        /// This sets up a promotion
-        /// It assumes OrgPrice was set 
-        /// </summary>
-        /// <param name="newPrice"></param>
-        /// <param name="promotionalText"></param>
-        /// <returns>string conatining error, or null if no error</returns>
-        public string AddPromotion(decimal newPrice, string promotionalText)
-        {
-            if (promotionalText == null)
-                return "You must provide some text to go with the promotion";
-            ActualPrice = newPrice;
-            PromotionalText = promotionalText;
-            return null;
-        }
 
-        public void RemovePromotion()
-        {
-            ActualPrice = OrgPrice;
-            PromotionalText = null;
-        }
     }
 
 }
