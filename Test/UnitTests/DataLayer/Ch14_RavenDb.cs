445b75d73e4855c804d95147fc9bc5faa526e54a
Jon Smith <jon.smith@selectiveanalytics.com>
2017-10-02 15:59:22 +0100

    Moved to Lazy connection setup

--- Test/UnitTests/DataLayer/Ch14_RavenDb.cs
+++ Test/UnitTests/DataLayer/Ch14_RavenDb.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2016 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT licence. See License.txt in the project root for license information.
 
+using System;
 using System.Linq;
 using DataLayer.NoSql;
 using Raven.Client.Documents;
@@ -15,24 +16,68 @@ namespace test.UnitTests.DataLayer
     public class Ch14_RavenDb
     {
         private readonly ITestOutputHelper _output;
-        private readonly IDocumentStore _store;
 
         private const string DatabaseName = "EfCoreInAction-Ch14-RavenDb";
+
+        private static readonly Lazy<IDocumentStore> theDocStore = new Lazy<IDocumentStore>(() =>
+        {
+            var store = new DocumentStore
+            {
+                Urls = new[] { RavenDbHelpers.RavenDbTestServerUrl },
+                Database = DatabaseName
+            }.Initialize();
+
+            new BookById().Execute(store);
+            new BookByActualPrice().Execute(store);
+            new BookByVotes().Execute(store);
+            if (store.NumEntriesInDb() <= 0)
+            {
+                store.SeedDummyBooks();
+            }
+            return store;
+        });
+
+        private IDocumentStore Store
+        {
+            get { return theDocStore.Value; }
+        }
 
         public Ch14_RavenDb(ITestOutputHelper output)
         {
             _output = output;
-            _store = new DocumentStore
+            
+        }
+
+        private class BookById : AbstractIndexCreationTask<BookNoSqlDto>
+        {
+            public BookById()
             {
-                Urls = new []{ RavenDbHelpers.RavenDbTestServerUrl},
-                Database = DatabaseName
-            }.Initialize();
-            new BookById().Execute(_store);
-            if (_store.NumEntriesInDb() <= 0)
-            {
-                _store.SeedDummyBooks();
+                Map = books => from book in books
+                    select new { book.Id };
+                Indexes.Add(x => x.Id, FieldIndexing.Exact);
             }
         }
+
+        private class BookByActualPrice : AbstractIndexCreationTask<BookNoSqlDto>
+        {
+            public BookByActualPrice()
+            {
+                Map = books => from book in books
+                    select new { book.ActualPrice };
+                Indexes.Add(x => x.Id, FieldIndexing.Default);
+            }
+        }
+
+        private class BookByVotes : AbstractIndexCreationTask<BookNoSqlDto>
+        {
+            public BookByVotes()
+            {
+                Map = books => from book in books
+                    select new { book.ReviewsAverageVotes };
+                Indexes.Add(x => x.Id, FieldIndexing.Default);
+            }
+        }
+
 
         [Fact]
         public void TestAccessDatabase()
@@ -40,7 +85,7 @@ namespace test.UnitTests.DataLayer
             //SETUP
 
             //ATTEMPT
-            var count = _store.NumEntriesInDb();
+            var count = Store.NumEntriesInDb();
 
             //VERIFY
             count.ShouldEqual(10);
@@ -50,7 +95,7 @@ namespace test.UnitTests.DataLayer
         public void TestQuery()
         {
             //SETUP
-            using (var session = _store.OpenSession())
+            using (var session = Store.OpenSession())
             {
                 //ATTEMPT
                 var data = session.Query<BookNoSqlDto>().ToList();
@@ -60,21 +105,11 @@ namespace test.UnitTests.DataLayer
             }
         }
 
-        private class BookById : AbstractIndexCreationTask<BookNoSqlDto>
-        {
-            public BookById()
-            {
-                Map = books => from book in books
-                               select new {book.Id};
-                Indexes.Add(x => x.Id, FieldIndexing.Exact);
-            }
-        }
-
         [Fact]
         public void TestQueryWithFilter()
         {
             //SETUP
-            using (var session = _store.OpenSession())
+            using (var session = Store.OpenSession())
             {
                 //ATTEMPT
                 var data = session.Query<BookNoSqlDto>()
@@ -88,10 +123,10 @@ namespace test.UnitTests.DataLayer
         }
 
         [Fact]
-        public void TestQueryWithSort()
+        public void TestQueryWithIndexSort()
         {
             //SETUP
-            using (var session = _store.OpenSession())
+            using (var session = Store.OpenSession())
             {
                 //ATTEMPT
                 var data = session.Query<BookNoSqlDto>().OrderByDescending(x => x.Id)
@@ -100,6 +135,39 @@ namespace test.UnitTests.DataLayer
                 //VERIFY
                 var i = 10;
                 data.ForEach(x => x.Id.ShouldEqual(BookNoSqlDto.ConvertIdToRavenId(i--)));
+            }
+        }
+
+        [Fact]
+        public void TestQueryWithPriceSort()
+        {
+            //SETUP
+            using (var session = Store.OpenSession())
+            {
+                //ATTEMPT
+                var data = session.Query<BookNoSqlDto>().OrderByDescending(x => x.ActualPrice)
+                    .ToList();
+
+                //VERIFY
+                var i = 10;
+                data.ForEach(x => x.ActualPrice.ShouldEqual(i--));
+            }
+        }
+
+        [Fact]
+        public void TestQuerySortAndFilter()
+        {
+            //SETUP
+            using (var session = Store.OpenSession())
+            {
+                //ATTEMPT
+                var data = session.Query<BookNoSqlDto>().OrderBy(x => x.ActualPrice)
+                    .Where(x => x.ReviewsAverageVotes > 2.75)
+                    .ToList();
+
+                //VERIFY
+                data.Count.ShouldEqual(2);
+                data.Select(x => x.Id).ShouldEqual(new []{BookNoSqlDto.ConvertIdToRavenId(6), BookNoSqlDto.ConvertIdToRavenId(10)});
             }
         }
     }
