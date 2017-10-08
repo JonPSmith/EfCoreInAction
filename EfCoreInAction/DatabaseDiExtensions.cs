89bcc99a36274c35709f2a3fa6592a1c83713d3b
Jon Smith <jon.smith@selectiveanalytics.com>
2017-10-08 17:51:46 +0100

    Moved database DI setup to new class

---
+++ EfCoreInAction/DatabaseDiExtensions.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+namespace EfCoreInAction
+{
+    public class DatabaseDiExtensions
+    {
+        
+    }
+}
