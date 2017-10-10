b36f6f46129119bb8c192d230ce4f61f94b1d4ac
Jon Smith <jon.smith@selectiveanalytics.com>
2017-10-10 21:29:46 +0100

    Updated Dapper - very small improvement

--- ServiceLayer/BookServices/DapperQueries/DapperQueries.cs
+++ ServiceLayer/BookServices/DapperQueries/DapperQueries.cs
@@ -159,21 +159,12 @@ AND ([b].[PublishedOn] <= GETUTCDATE()) ";
                 : "SELECT ";
 
             return selectOpt +
-@"[b].[BookId], [b].[Title], [b].[Price], [b].[PublishedOn], CASE
-    WHEN [p.Promotion].[PriceOfferId] IS NULL
-    THEN [b].[Price] ELSE [p.Promotion].[NewPrice]
-END AS [ActualPrice], 
+@"[b].[BookId], [b].[Title], [b].[Price], [b].[PublishedOn],
+COALESCE([p.Promotion].[NewPrice], [b].[Price]) AS [ActualPrice],  
 [p.Promotion].[PromotionalText] AS [PromotionPromotionalText], 
 [dbo].AuthorsStringUdf([b].[BookId]) AS [AuthorsOrdered], 
-(
-    SELECT COUNT(*)
-    FROM [Review] AS [r]
-    WHERE [b].[BookId] = [r].[BookId]
-) AS [ReviewsCount], (
-    SELECT AVG(CAST([y].[NumStars] AS float))
-    FROM [Review] AS [y]
-    WHERE [b].[BookId] = [y].[BookId]
-) AS [ReviewsAverageVotes]
+( SELECT COUNT(*) FROM [Review] AS [r] WHERE [b].[BookId] = [r].[BookId] ) AS [ReviewsCount], 
+( SELECT AVG(CAST([y].[NumStars] AS float)) FROM [Review] AS [y] WHERE [b].[BookId] = [y].[BookId] ) AS [ReviewsAverageVotes] 
 FROM [Books] AS [b]
 LEFT JOIN [PriceOffers] AS [p.Promotion] ON [b].[BookId] = [p.Promotion].[BookId]
 ";
