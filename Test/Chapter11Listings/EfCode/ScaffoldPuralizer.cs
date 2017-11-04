85c6fd07740eaf3b1b7cc0884bfe556a88d475e1
Jon Smith <jon.smith@selectiveanalytics.com>
2017-11-04 14:47:26 +0000

    Added annotations

--- Test/Chapter11Listings/EfCode/ScaffoldPuralizer.cs
+++ Test/Chapter11Listings/EfCode/ScaffoldPuralizer.cs
@@ -32,6 +32,11 @@ namespace Test.Chapter11Listings.EfCode
         }                                     //#F
     }
     /*************************************************************************
-     * 
+    #A The design time methods look for a class which implements the IDesignTimeServices interface. If one exists it will call its method ConfigureDesignTimeServices
+    #B This method is here to allow other services to be added to the design time services
+    #C I add my puralizer, which will replace the default, noop, puralizer
+    #D This is my implementation of the IPluralizer interface. In this case I'm only really interested in the Singularize method, but I implement both
+    #E This takes a name and puralize it, for instance, dog would become dogs. I use a small .NET 4.5 NuGet library called Inflector
+    #E This takes a name and singulaizes it, that is, cats would become cat.
      * ***********************************************************************/
 }
